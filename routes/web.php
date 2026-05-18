7b181b311a8dcaa052a77dbf757c29b70cf138fb
ASUS <mohammedkhamash3@gmail.com>
2026-05-18 14:11:12 +0300

    Update project

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,14 @@ Route::post('/about', function () {
 
     return view('about', compact('name', 'departments'));
 });
+
+Route::get('tasks', function(){
+    return view('tasks');
+});
+
+Route::post('create', function() {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+
+    return view('tasks');
+});
