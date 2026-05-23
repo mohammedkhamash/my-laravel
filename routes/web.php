b30435c1db662da6be099e44c0f1dccba8b32e65
ASUS <mohammedkhamash3@gmail.com>
2026-05-23 16:53:46 +0300

    Third update

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,35 @@ Route::post('/about', function () {
 });
 
 Route::get('tasks', function(){
-    return view('tasks');
+
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('tasks'));
 });
 
 Route::post('create', function() {
     $task_name = $_POST['name'];
     DB::table('tasks')->insert(['name' => $task_name]);
 
-    return view('tasks');
+   return redirect()->back();
+});
+
+Route::post('delete/{id}', function($id) {
+    DB::table('tasks')->where('id', $id)->delete();
+
+    return redirect()->back();
+});
+
+Route::post('edit/{id}', function($id) {
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks', compact('task', 'tasks'));
+});
+
+Route::post('update', function() {
+    $id = $_POST['id'];
+
+    DB::table('tasks')->where('id', '=', $id)->update(['name' => $_POST['name']]);
+
+    return redirect('tasks');
 });
